6ecba1f00d4b7dfa4ac5756069847de1be8b5e5f
TrestoXD <vitogamer08@gmail.com>
2025-09-30 16:41:33 +0200

    cheese

--- Portafolio/Windows.js
+++ Portafolio/Windows.js
@@ -1,12 +1,28 @@
-import Konva from 'konva';
+let newX = 0, newY = 0, startX = 0, startY = 0;
 
-const stage = new Konva.Sage({
-    container: 'container',
-    width: window.innerWidth,
-    height: window.innerHeight,
-});
+const card = document.getElementById('card')
 
-const layer = new Konva.Layer();
-stage.add(layer);
+card.addEventListener('mousedown', mouseDown)
 
-const
+function mouseDown(e){
+    startX = e.clientX
+    startY = e.clientY
+
+    document.addEventListener('mousemove', mouseMove)
+    document.addEventListener('mouseup', mouseUp)
+}
+
+function mouseMove(e){
+    newX = startX - e.clientX 
+    newY = startY - e.clientY 
+  
+    startX = e.clientX
+    startY = e.clientY
+
+    card.style.top = (card.offsetTop - newY) + 'px'
+    card.style.left = (card.offsetLeft - newX) + 'px'
+}
+
+function mouseUp(e){
+    document.removeEventListener('mousemove', mouseMove)
+}
