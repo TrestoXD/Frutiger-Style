c3fe2bd4524e19f99ecd18055c559ae90274f4d1
TrestoXD <vitogamer08@gmail.com>
2025-10-14 18:38:22 +0200

    1.0.3
    Mejorando botones

--- Portafolio/icons.js
+++ Portafolio/icons.js
@@ -1,6 +1,7 @@
 let icoX = 0, icoY = 0, icostartX = 0, icostartY = 0;
 
 let icona = document.querySelectorAll('#icos');
+let draggable = true;
 
 icona.forEach(function(elem){
    elem.addEventListener('mousedown', mouseDownicons);
@@ -21,16 +22,18 @@ function mouseHover(e){
     e.target.style.backgroundColor = 'rgba(255, 255, 255, 0.199)';
     e.target.style.borderStyle = 'solid';
     e.target.style.borderColor = '#00000067';
+    draggable = true;
 }
 
 function mouseOut(e){
     e.target.style.backgroundColor = 'rgba(255, 255, 255, 0)';
     e.target.style.borderStyle = 'solid';
     e.target.style.borderColor = '#00000000';
+    draggable = false;
 }
 
 function mouseMoveicons(e){
-    if(e.target.closest(".item-papelera")){
+    if(e.target.closest(".item-papelera") && draggable == true){
         console.log("yes")
         icoX = icostartX - e.clientX 
         icoY = icostartY - e.clientY 
@@ -42,10 +45,15 @@ function mouseMoveicons(e){
         e.target.style.left = (e.target.offsetLeft - icoX) + 'px'
         e.target.style.opacity = 0.5;
     }
+    else{
+        console.log("ey");
+        mouseUpicons(e);
+    }
 }
 
 function mouseUpicons(e){
     document.removeEventListener('mousemove', mouseMoveicons)
+    document.removeEventListener('mousedown', mouseDownicons)
     icoX = icostartX - e.clientX 
     icoY = icostartY - e.clientY 
         
@@ -53,7 +61,8 @@ function mouseUpicons(e){
     icostartY = e.clientY
 
     e.target.style.top = (Math.floor((e.target.offsetTop - icoY) /100))*100 + 10 +'px'
-    e.target.style.left = (Math.floor((e.target.offsetLeft - icoX) /100 ))*100 + 10 + 'px'
+    e.target.style.left = (Math.floor((e.target.offsetLeft - icoX) /100 ))*100 + 5 + 'px'
+    e.target.querySelector()
     e.target.style.opacity = 1;
-    console.log("END")
+    console.log("END");
 }
