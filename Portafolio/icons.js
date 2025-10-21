efa9029dc8c433125b63bcc0d58b42a11dc7008f
TrestoXD <vitogamer08@gmail.com>
2025-10-21 14:11:47 +0200

    Beta 1.1.0
    FINALLY ICONS OF DESKTOP FINISHED!

--- Portafolio/icons.js
+++ Portafolio/icons.js
@@ -1,6 +1,8 @@
 let icoX = 0, icoY = 0, icostartX = 0, icostartY = 0;
 
-let icona = document.querySelectorAll('#icos');
+const ico1 = document.getElementById('icos1'), ico2 = document.getElementById('icos2'), ico3 = document.getElementById('icos3'), ico4 = document.getElementById('icos4'), ico5 = document.getElementById('icos5'), ico6 = document.getElementById('icos6'), ico7 = document.getElementById('icos7')
+
+let icona = document.querySelectorAll('.item-papelera');
 let draggable = true;
 
 icona.forEach(function(elem){
@@ -43,7 +45,27 @@ function mouseMoveicons(e){
 
         e.target.style.top = (e.target.offsetTop - icoY) + 'px'
         e.target.style.left = (e.target.offsetLeft - icoX) + 'px'
-        this.getElementById('icos').classList.add('moving')
+        if(e.target == ico1){
+            e.target.classList.add('moving');
+        }
+        else if (e.target == ico2){
+            e.target.classList.add('moving');
+        }
+        else if (e.target == ico3){
+            e.target.classList.add('moving');
+        }
+        else if (e.target == ico4){
+            e.target.classList.add('moving');
+        }
+        else if (e.target == ico5){
+            e.target.classList.add('moving');
+        }
+        else if (e.target == ico6){
+            e.target.classList.add('moving');
+        }
+        else if (e.target == ico7){
+            e.target.classList.add('moving');
+        }
     }
     else{
         console.log("ey");
@@ -62,6 +84,28 @@ function mouseUpicons(e){
 
     e.target.style.top = (Math.floor((e.target.offsetTop - icoY) /100))*100 + 10 +'px'
     e.target.style.left = (Math.floor((e.target.offsetLeft - icoX) /100 ))*100 + 5 + 'px'
-    document.getElementById('icos').classList.remove('moving')
+    if(e.target == ico1){
+        e.target.classList.remove('moving');
+        console.log("back to normal 1")
+    }
+    else if(e.target == ico2){
+        e.target.classList.remove('moving');
+        console.log("back to normal 2")
+    }
+    else if(e.target == ico3){
+        e.target.classList.remove('moving');
+    }
+    else if(e.target == ico4){
+        e.target.classList.remove('moving');
+    }
+    else if(e.target == ico5){
+        e.target.classList.remove('moving');
+    }
+    else if(e.target == ico6){
+        e.target.classList.remove('moving');
+    }
+    else if(e.target == ico7){
+        e.target.classList.remove('moving');
+    }
     console.log("END");
 }
