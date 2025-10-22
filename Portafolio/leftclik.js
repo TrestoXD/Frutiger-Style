db776fa46eaad1ca7d3cd407831f1779b8a279ef
TrestoXD <vitogamer08@gmail.com>
2025-10-22 19:22:37 +0200

    Beta 1.1.2
    cHILINDRINA

--- Portafolio/leftclik.js
+++ Portafolio/leftclik.js
@@ -1,9 +1,20 @@
 oncontextmenu = (e) => {
-    e.preventDefault()
-    let menu = document.createElement("div")
-    menu.id = "rightmenu"
-    menu.style = `top:${e.pageY-10}px;left:${e.pageX-40}px`
-    menu.onmouseleave = () => rightmenu.outerHTML = ''
-    menu.innerHTML = "<p>Open</p><p>Add to Zip</p><hr></hr> <p>Send To</p> <hr></hr> <p>Cut</p><p>Copy</p> <hr></hr> <p>Create Shotcut</p> <p>Delete</p> <p>Rename</p> <hr></hr> <p>Properties</p>"
-    document.body.appendChild(menu)
+    let rightmenudiv = document.getElementById("rightmenu");
+    if(rightmenudiv === null){
+        e.preventDefault();
+        let menu = document.createElement("div")
+        menu.id = "rightmenu"
+        menu.style = `top:${e.pageY-10}px;left:${e.pageX-40}px`
+        if(menu.parentNode){
+            menu.parentNode.removeChild(menu);
+        }
+        menu.onmouseleave = () => rightmenu.outerHTML = '', onmenu = false;
+        if(e.target.closest(".item-papelera"))
+        menu.innerHTML = "<p>Open</p><p>Add to Zip</p><hr></hr> <p>Send To</p> <hr></hr> <p>Cut</p><p>Copy</p> <hr></hr> <p>Create Shotcut</p> <p>Delete</p> <p>Rename</p> <hr></hr> <p>Properties</p>"
+        else{
+        menu.innerHTML = "<p>Arrange Icons</p><p>Line Up Icons</p> <hr></hr> <p>Paste</p><p>Paste Shortcut</p><p>Undo Delete</p> <hr></hr> <p>New</p> <hr></hr> <p>Properties</p>"
+        }
+        document.body.appendChild(menu)
+    }
 }
+
