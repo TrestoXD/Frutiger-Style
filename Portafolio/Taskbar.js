b6adb5ff4a937ccddb7f876621e815052c7c1673
TrestoXD <vitogamer08@gmail.com>
2025-11-24 18:51:04 +0100

    Taskbar fixed!

--- Portafolio/Taskbar.js
+++ Portafolio/Taskbar.js
@@ -10,31 +10,32 @@ else if(document.getElementById('sidemen').classList.contains('overlay_taskbar')
 
 function sidebarmenu(){
     if(document.getElementById('sidemen').classList.contains('overlay_taskbar') && meg == false){
-        //console.log("On");  
+        console.log("On");  
         document.getElementById('sidemen').classList.add('active');
         meg = true;
     }
     else if(document.getElementById('sidemen').classList.contains('overlay_taskbar') && meg == true){
-        //console.log("Off");
+        console.log("Off");
         document.getElementById('sidemen').classList.remove('active');
         meg = false;
     }
     //console.log(meg);
 }
 
-addEventListener("mousedown", (e) => {
+addEventListener("mouseup", (e) => {
     //if(e.target.classList.contains('overlay_taskbar')){
     //    console.log("Detectado");
     //}
     let current = e.target.getAttribute('class');
     //console.log(current)
 
-    if(e.target.getAttribute('class') === "content-inside-95" || e.target.getAttribute("class") === "windows95-taskbar-menu-title"){
-        //console.log("si");
+    if(e.target.getAttribute('class') === "content-inside-95" || e.target.getAttribute("class") === "windows95-taskbar-menu-title" || e.target.getAttribute("id") === "buttonstarting"){
+        console.log("si");
     }
     else{
         document.getElementById('sidemen').classList.remove('active');
         meg = false;
-        //console.log("no");
+        console.log("no");
     }
-})
+}
+)
