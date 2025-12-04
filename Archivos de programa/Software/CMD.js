06938ced054543c9c943e074b48a208f6fd59070
TrestoXD <vitogamer08@gmail.com>
2025-12-04 16:44:24 +0100

    working on the open software

--- Archivos de programa/Software/CMD.js
+++ Archivos de programa/Software/CMD.js
@@ -1,5 +1,16 @@
 console.log(totalapp)
 
-function opensoftware(){
-    
+let task = document.createElement("div")
+
+function opensoftware(id, xcord, ycord, htmlint){
+    totalapp++;
+    if(document.getElementById('task' + totalapp) == null){
+        task.id = id;
+        task.style = `bottom:${ycord}px;left:${xcord}px`
+        if(task.parentNode){
+            task.parentNode.removeChild(task);
+        }
+        task.innerHTML = `<iframe src='Archivos de programa\Software${htmlint}'></iframe>`
+        document.body.appendChild(task)
+    }
 }
